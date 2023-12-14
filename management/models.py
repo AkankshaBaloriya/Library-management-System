514fea789666db8106c457ec76caacb5ad032882
AkankshaBaloriya <baloriyaakanksha@gmail.com>
2023-12-14 22:09:23 +0530

    add book function completed

--- management/models.py
+++ management/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Add(models.Model):
-    name=models.CharField(max_length=50)
+    name=models.CharField(max_length=50,default=None)
     price=models.IntegerField(default=0)
-    author=models.CharField(max_length=20)
-    description=models.TextField(max_length=100)
+    author=models.CharField(max_length=20, default=None)
+    description=models.TextField(max_length=100, default=None)
+    image=models.ImageField(upload_to='books', default='default.jpg')
     
-    def addit(self):
-        self.save()
+    def __str__(self):
+        return self.name
+    
